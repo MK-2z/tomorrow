336c3789cce2ad8317a7ee732cc23ad6a0c4cea2
MK-2z <2305008670@qq.com>
2026-09-05 08:30:16 +0800

    Add '@server' alias to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,13 @@ export default defineConfig({
     emptyOutDir: true,
   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'client/src'),
-      '@client': path.resolve(__dirname, 'client/src'),
-      '@shared': path.resolve(__dirname, 'shared'),
-    },
+  alias: {
+    '@': path.resolve(__dirname, 'client/src'),
+    '@client': path.resolve(__dirname, 'client'),
+    '@server': path.resolve(__dirname, 'server'),
+    '@shared': path.resolve(__dirname, 'shared'),
   },
+},
   server: {
     port: 5173,
     proxy: {
